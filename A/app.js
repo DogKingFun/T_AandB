4d59d173934f7391db9ee843156ef20b5b7c541a
DogKingFun <108065335+DogKingFun@users.noreply.github.com>
2022-08-20 21:07:47 +0900

    Update app.js

--- A/app.js
+++ A/app.js
@@ -12,22 +12,3 @@ const server = net.createServer(socket => {
 }).listen(3000);
 
 console.log('listening on port 3000');
-
-/*const dgram = require('dgram');
-
-const PORT_A = 8000;
-const HOST_A = 'a';
-
-const PORT_B = 8001;
-const HOST_B = 'b';
-
-const socket = dgram.createSocket('udp4');
-
-socket.on('message', (buf, rinfo) => {
-    console.log(rinfo.address + ':' + rinfo.port +' - ' + buf);
-    socket.send(buf, 0, buf.length, PORT_B, HOST_B, (err, bytes) => {
-        if (err) throw err;
-    });
-});
-
-socket.bind(PORT_A, HOST_A);*/
